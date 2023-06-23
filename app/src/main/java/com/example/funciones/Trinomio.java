5e3539b0428f7757a0cd07703b2ae0ac245665fb
PamelaVargas2000 <ana.vargas794@ist17dejulio.edu.ec>
2023-06-22 20:14:44 -0500

    crear la pantalla dl Trinomi Cuadrado Pecfecto

---
+++ app/src/main/java/com/example/funciones/Trinomio.java
@@ -0,0 +1,14 @@
+package com.example.funciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Trinomio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trinomio);
+    }
+}
